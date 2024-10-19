1a844a2aa6b40afe1220e671b2af303c51bcfd34
MarwinSoares <123134770+MarwinSoares@users.noreply.github.com>
2024-10-19 19:28:24 -0300

    upload

--- sistema_arrecadacao.sql
+++ sistema_arrecadacao.sql
@@ -64,7 +64,6 @@ DROP TABLE IF EXISTS `doadores`;
 CREATE TABLE IF NOT EXISTS `doadores` (
   `id_doador` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
-  `cpf` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `telefone` varchar(11) DEFAULT NULL,
   PRIMARY KEY (`id_doador`)
